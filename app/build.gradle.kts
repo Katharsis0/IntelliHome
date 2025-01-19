6e213aabc54fe13914ce8a2f0c7e7e1dff931219
Sebastián Hidalgo Vargas <55753352+Katharsis0@users.noreply.github.com>
2025-01-19 14:34:36 -0600

    Toggle visibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,11 +32,11 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation ("com.google.android.material:material:1.9.0")  //Material Icons library
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
