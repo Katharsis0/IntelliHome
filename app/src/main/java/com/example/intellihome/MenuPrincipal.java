9fb5d90aaa1cd80e2e9f77bd4f031ceebc11422e
Meibel Ceciliano <cecilianomeibel@gmail.com>
2025-01-24 00:02:30 -0600

    Se crea vista de menú principal y evento de salir

---
+++ app/src/main/java/com/example/intellihome/MenuPrincipal.java
@@ -0,0 +1,34 @@
+package com.example.intellihome;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MenuPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_menu_principal);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        // Obtener el botón y configurar el listener
+        ImageButton button = findViewById(R.id.salir);
+        button.setOnClickListener(v -> {
+            // Redirigir a la actividad OlvidoContrasena
+            Intent intent = new Intent(MenuPrincipal.this, MainActivity.class);
+            startActivity(intent);
+        });
+    }
+}
